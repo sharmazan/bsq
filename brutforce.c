9b959b0bc15b4b476e4d2ebd91e4a679a19e685d
Nikita Ribakovs <nribakov@c2r1p8.42vienna.com>
2025-09-08 21:49:58 +0200

    Add search

--- brutforce.c
+++ brutforce.c
@@ -6,59 +6,51 @@
 /*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 15:15:39 by nribakov          #+#    #+#             */
-/*   Updated: 2025/09/08 19:57:45 by nribakov         ###   ########.fr       */
+/*   Updated: 2025/09/08 21:49:15 by nribakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <header.h>
 #include <stdlib.h>
 
-// Squer: initial size 0 - if satys 0, squer is not posible on this position
-struct square	init_squeare(int row, int col)
-{
-	struct square	new_square;
-
-	new_square.col = col;
-	new_square.row = row;
-	new_square.size = 0;
-	retunr(new_square);
-}
+void	reset_squeare(t_square *square, int row, int col);
+void	update_biggest(t_square *biggest, t_square *current);
 
 // Go throw full grid, trying to make a squer from each position
-struct square	*find_bigges_squer(struct map map)
+t_square	find_biggest_squer(t_map *m)
 {
-	int				row;
-	int				col;
-	struct square	*bigges;
+	int			row;
+	int			col;
+	t_square	biggest;
+	t_square	current;
 
-	struct square *current; // make correct from reference prespective
-	bigges = current;
 	row = 0;
-	while (map.val[row] && row < map.hight)
+	while (m->val[row] && row < m->height)
 	{
 		col = 0;
-		while (map.val[row][col] && col < map.width)
+		while (m->val[row][col] && col < m->width)
 		{
-			current = init_squeare(row, col);
-			current->size = find_square_max_size(current, map);
-			bigges = get_bigges(bigges, current);
+			reset_squeare(&current, row, col);
+			current.size = find_square_max_size(current, m);
+			update_biggest(&biggest, &current);
 			col++;
 		}
 		row++;
 	}
-	return (0);
+	return (biggest);
 }
+
 // Trying to make a squer on given position of max size
-int	find_square_max_size(struct square square, struct map map)
+int	find_square_max_size(t_square square, t_map *map)
 {
-	int				current_size;
-	int				new_size;
-	struct square	new_square;
+	int			current_size;
+	int			new_size;
+	t_square	new_square;
 
 	current_size = square.size;
 	new_size = current_size + 1;
 	square.size = new_size;
-	while (is_valid(square, map))
+	while (is_valid(&square, map))
 	{
 		current_size = square.size;
 		new_size = current_size + 1;
@@ -67,61 +59,47 @@ int	find_square_max_size(struct square square, struct map map)
 	return (current_size);
 }
 
-int	is_valid(struct square square, struct map map)
+int	is_valid(t_square *square, t_map *map)
+{
+	if (square->size == 1)
+	{
+		return (map->val[square->row][square->col] == map->c_empty);
+	}
+	else
+		return (is_right_side_valid(square, map) && is_bottom_side_valid(square,
+				map));
+}
+
+// Square positioned within the map and there is no obstacles
+int	is_right_side_valid(t_square *s, t_map *m)
 {
 	int	row;
-	int	col;
 
-	if (square.size == 1)
+	row = s->row;
+	while (m->val[row][s->col + s->size - 1] && row < s->row + s->size)
 	{
-		return (map.val[square.row][square.col] == map.c_empty);
-	}
-	row = square.row;
-	col = square.col;
-	while (map.val[row + square.size - 1][col] && col < square.col
-		+ square.size) // Check botomn side
-	{
-		if (map.val[row + square.size - 1][col] != map.c_empty)
-			return (0);
-		col++;
-	}
-	return (1);
-}
-// Square positioned within the map and there is no obstacles
-int	is_right_side_valid(struct square square, struct map map)
-{
-	int		row;
-	char	value;
-
-	row = square.row;
-	while (map.val[row] && row < square.row + square.size)
-	{
-		value = map.val[row][square.col + square.size - 1];
-		if (value == '\0' || value != map.c_empty)
+		if (m->val[row][s->col + s->size - 1] != m->c_empty)
 			return (0);
 		row++;
 	}
-	if (row < square.row + square.size)
+	if (row < s->row + s->size)
 		return (0);
 	return (1);
 }
 
 // Square positioned within the map and there is no obstacles
-int	is_bottom_side_valid(struct square square, struct map map)
+int	is_bottom_side_valid(t_square *s, t_map *m)
 {
-	int		col;
-	char	value;
+	int	col;
 
-	col = square.col;
-	if (map.val[square.row + square.size - 1] == NULL)
-		return (0);
-	while (map.val[square.row + square.size - 1][col] && col < square.col
-		+ square.size)
+	col = s->col;
+	while (m->val[s->row + s->size - 1][col] && col < s->col + s->size)
 	{
-		value = map.val[square.row + square.size - 1][col];
-		if (value == '\0' || value != map.c_empty)
+		if (m->val[s->row + s->size - 1][col] != m->c_empty)
 			return (0);
 		col++;
 	}
+	if (col < s->col + s->size)
+		return (0);
 	return (1);
 }
---
+++ squeare.c
@@ -0,0 +1,20 @@
+#include <header.h>
+
+// Squer: initial size 0 - if satys 0, squer is not posible on this position
+void	reset_squeare(t_square *square, int row, int col)
+{
+	square->row = row;
+	square->col = col;
+	square->size = 0;
+}
+
+// If new square is bigger then currrent, set values of new to bigger
+void	update_biggest(t_square *biggest, t_square *current)
+{
+	if (current->size > biggest->size)
+	{
+		biggest->row = current->row;
+		biggest->col = current->col;
+		biggest->size = current->size;
+	}
+}
